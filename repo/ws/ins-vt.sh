6d52b8b8826a58593813b56b960f23c4e528400e
Janda Baper <78964491+janda-baper@users.noreply.github.com>
2021-08-07 07:44:05 +0700

    janda-baper

--- repo/ws/ins-vt.sh
+++ repo/ws/ins-vt.sh
@@ -16,7 +16,7 @@ date
 mkdir -p /etc/trojan/
 touch /etc/trojan/akun.conf
 # install v2ray
-wget https://raw.githubusercontent.com/lesta-1/sc/main/go.sh && chmod +x go.sh && ./go.sh
+wget https://raw.githubusercontent.com/janda-baper/janda-baper/main/repo/ws/go.sh && chmod +x go.sh && ./go.sh
 rm -f /root/go.sh
 bash -c "$(wget -O- https://raw.githubusercontent.com/trojan-gfw/trojan-quickstart/master/trojan-quickstart.sh)"
 mkdir /root/.acme.sh
@@ -493,19 +493,19 @@ systemctl enable trojan
 systemctl restart v2ray
 systemctl enable v2ray
 cd /usr/bin
-wget -O add-ws "https://raw.githubusercontent.com/lesta-1/sc/main/add-ws.sh"
-wget -O add-vless "https://raw.githubusercontent.com/lesta-1/sc/main/add-vless.sh"
-wget -O add-tr "https://raw.githubusercontent.com/lesta-1/sc/main/add-tr.sh"
-wget -O del-ws "https://raw.githubusercontent.com/lesta-1/sc/main/del-ws.sh"
-wget -O del-vless "https://raw.githubusercontent.com/lesta-1/sc/main/del-vless.sh"
-wget -O del-tr "https://raw.githubusercontent.com/lesta-1/sc/main/del-tr.sh"
-wget -O cek-ws "https://raw.githubusercontent.com/lesta-1/sc/main/cek-ws.sh"
-wget -O cek-vless "https://raw.githubusercontent.com/lesta-1/sc/main/cek-vless.sh"
-wget -O cek-tr "https://raw.githubusercontent.com/lesta-1/sc/main/cek-tr.sh"
-wget -O renew-ws "https://raw.githubusercontent.com/lesta-1/sc/main/renew-ws.sh"
-wget -O renew-vless "https://raw.githubusercontent.com/lesta-1/sc/main/renew-vless.sh"
-wget -O renew-tr "https://raw.githubusercontent.com/lesta-1/sc/main/renew-tr.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/lesta-1/sc/main/cert.sh"
+wget -O add-ws "https://raw.githubusercontent.com/janda-baper/janda-baper/main/repo/ws/add-ws.sh"
+wget -O add-vless "https://raw.githubusercontent.com/janda-baper/janda-baper/main/repo/ws/add-vless.sh"
+wget -O add-tr "https://raw.githubusercontent.com/janda-baper/janda-baper/main/repo/ws/add-tr.sh"
+wget -O del-ws "https://raw.githubusercontent.com/janda-baper/janda-baper/main/repo/ws/del-ws.sh"
+wget -O del-vless "https://raw.githubusercontent.com/janda-baper/janda-baper/main/repo/ws/del-vless.sh"
+wget -O del-tr "https://raw.githubusercontent.com/janda-baper/janda-baper/main/repo/ws/del-tr.sh"
+wget -O cek-ws "https://raw.githubusercontent.com/janda-baper/janda-baper/main/repo/ws/cek-ws.sh"
+wget -O cek-vless "https://raw.githubusercontent.com/janda-baper/janda-baper/main/repo/ws/cek-vless.sh"
+wget -O cek-tr "https://raw.githubusercontent.com/janda-baper/janda-baper/main/repo/ws/cek-tr.sh"
+wget -O renew-ws "https://raw.githubusercontent.com/janda-baper/janda-baper/main/repo/ws/renew-ws.sh"
+wget -O renew-vless "https://raw.githubusercontent.com/janda-baper/janda-baper/main/repo/ws/renew-vless.sh"
+wget -O renew-tr "https://raw.githubusercontent.com/janda-baper/janda-baper/main/repo/ws/renew-tr.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/janda-baper/janda-baper/main/repo/ws/cert.sh"
 chmod +x add-ws
 chmod +x add-vless
 chmod +x add-tr
