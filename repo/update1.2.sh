37dec8c4b70e37abf36fb8db7595b5b65a5d9e86
Janda Baper <78964491+janda-baper@users.noreply.github.com>
2021-08-31 22:13:59 +0700

    Update update1.2.sh

--- repo/update1.2.sh
+++ repo/update1.2.sh
@@ -11,7 +11,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
-IZIN=$( curl http://akses.rpj08.my.id:81/akses | grep $MYIP )
+IZIN=$( curl https://www.ipang.me/index.html | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
