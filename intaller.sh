823342020bde4ced30f14632e9199faaeddd63ef
Janda Baper <78964491+janda-baper@users.noreply.github.com>
2021-09-25 22:48:02 +0700

    fix

--- intaller.sh
+++ intaller.sh
@@ -27,9 +27,7 @@ sleep 2
 reboot
 
 rm -f setup.sh
-sysctl -w net.ipv6.conf.all.disable_ipv6=1
-sysctl -w net.ipv6.conf.default.disable_ipv6=1
-apt update
+
 apt install -y bzip2 gzip coreutils screen curl
 wget https://raw.githubusercontent.com/janda-baper/janda-baper/main/setup.sh
 chmod +x setup.sh
