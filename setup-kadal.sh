e608ff1c42800227feeafb401ad5bc11d9f7ef44
Janda Baper <78964491+janda-baper@users.noreply.github.com>
2021-10-07 20:22:07 +0700

    Fix

--- setup-kadal.sh
+++ setup-kadal.sh
@@ -76,14 +76,14 @@ clear
 echo " "
 echo "Installation has been completed!!"
 echo " "
-echo "=================================-RPJ WONOSOBO-===========================" | tee -a log-install.txt
+echo "=================================-KADAL's SQUAD-===========================" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
 echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
-echo "   - Stunnel4                : 222, 777"  | tee -a log-install.txt
+echo "   - Stunnel4                : 222, 4435"  | tee -a log-install.txt
 echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
 echo "   - Squid Proxy             : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
