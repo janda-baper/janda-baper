4f949ece91cab30750b82062d247a3a2abc95489
Janda Baper <78964491+janda-baper@users.noreply.github.com>
2021-10-08 14:10:03 +0700

    Fix

--- intall.sh
+++ intall.sh
@@ -26,9 +26,11 @@ update-grub
 sleep 2
 reboot
 
-rm -f setup.sh
-apt install -y bzip2 gzip coreutils screen curl
-wget https://raw.githubusercontent.com/janda-baper/janda-baper/main/setup.sh
-chmod +x setup.sh
-bash setup.sh
-rm -f setup.sh
+rm -f setup-kadal.sh
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+apt update
+wget https://raw.githubusercontent.com/janda-baper/janda-baper/main/setup-kadal.sh
+chmod +x setup-kadal.sh
+bash setup-kadal.sh
+rm -f setup-kadal.sh
