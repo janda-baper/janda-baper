6de70ec18fb54f36d3868eafe34ac61fe657dd4e
Janda Baper <78964491+janda-baper@users.noreply.github.com>
2021-10-22 22:02:35 +0700

    Update ssrmu.sh

--- repo/ssr/ssrmu.sh
+++ repo/ssr/ssrmu.sh
@@ -10,7 +10,7 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
 echo "Checking VPS"
-IZIN=$( curl https://www.ipang.me/ip | grep $MYIP )
+IZIN=$( curl https://ipang.me/ip/index.html | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
