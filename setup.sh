e222054e3bf71601ba7c770f72c7adb94c7dbbea
Janda Baper <78964491+janda-baper@users.noreply.github.com>
2021-10-22 21:57:15 +0700

    Fix

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
-IZIN=$( curl https://www.ipang.me/ip/index.html | grep $MYIP )
+IZIN=$( curl https://ipang.me/ip/index.html | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
