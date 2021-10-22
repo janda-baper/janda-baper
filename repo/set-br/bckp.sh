13c5d888a0d42fb74827e1407b8845221dcd0b0d
Janda Baper <78964491+janda-baper@users.noreply.github.com>
2021-10-22 20:40:13 +0700

    Fix

--- repo/set-br/bckp.sh
+++ repo/set-br/bckp.sh
@@ -4,7 +4,7 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://www.ipang.me/index.html | grep $MYIP )
+IZIN=$( curl https://www.ipang.me/ip | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
